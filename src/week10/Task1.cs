315acdee17391d5ba2cd32074e6542ced5b5c576
Danila <dds2412@mail.ru>
2019-05-21 12:05:13 +0300

    Добавлена десятая неделя

---
+++ src/week10/Task1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace Week10
+{
+    class Task1
+    {
+        static void Mzin1(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("output.txt"))
+            {
+                string[] inputString = File.ReadAllLines("input.txt");
+                int[] prefixFunction = getPrefixFunction(inputString[0]);
+
+                for (int i = 0; i < prefixFunction.Length; i++)
+                {
+                    sw.Write(prefixFunction[i] + " ");
+                }
+            }
+        }
+
+        private static int[] getPrefixFunction(String text)
+        {
+            var prefixFunction = new int[text.Length]; 
+
+            for (int i = 1; i < text.Length; i++)
+            {
+                int j = prefixFunction[i - 1]; //текущая длина префикса, который мы хотим продолжить
+                 
+                while (j > 0 && text[i] != text[j]) //пока мы не можем продолжить текущий префикс
+                {
+                    j = prefixFunction[j - 1]; //уменьшаем его длину до следующей возможной
+                }
+
+                //Теперь j - максимальная длина префикса, который мы можем продолжить,
+                //или 0, если такового не существует.
+                if (text[i] == text[j])
+                {
+                    prefixFunction[i] = j + 1;
+                }
+                else
+                {
+                    prefixFunction[i] = j;
+                }
+            }
+            return prefixFunction;
+        }
+    }
+}
---
+++ src/week10/Task2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Week10
+{
+    class Task2
+    {
+        static void Main2(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("output.txt"))
+            {
+                string[] inputString = File.ReadAllLines("input.txt");
+                int[] zFunction = getZFunction(inputString[0]);
+
+                for (int i = 1; i < zFunction.Length; i++)
+                {
+                    sw.Write(zFunction[i] + " ");
+                }
+            }
+        }
+
+        private static int[] getZFunction(String text)
+        {
+            var lenght = text.Length;
+            var zFunction = new int[lenght];
+
+            for(int i = 1, l = 0, r = 0; i  < lenght; i++)
+            {
+                if(i <= r)
+                {
+                    zFunction[i] = Math.Min(r - i + 1, zFunction[i - l]);
+                }
+                while(i+zFunction[i] < lenght && text[zFunction[i]] == text[i+ zFunction[i]]) { zFunction[i]++; }
+                if(i + zFunction[i] - 1 > r)
+                {
+                    l = i;
+                    r = i + zFunction[i] - 1;
+                }
+            }
+
+            return zFunction;
+        }
+    }
+}
